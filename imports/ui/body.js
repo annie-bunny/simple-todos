ac2b94655f7659922ca46ecb56413d6c0f1dad8f
annie-bunny <annie.bunny@bk.ru>
2016-11-09 09:31:34 +0300

    5.4  Import Task component from the body

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,40 +1,32 @@
 import { Template } from 'meteor/templating';
- 
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
 import { Tasks } from '../api/tasks.js';
  
+import './task.js';
 import './body.html';
  
 Template.body.helpers({
-  tasks() {
-    return Tasks.find({});
-  },
-});
-
-
-Template.body.events({
-  'submit .new-task'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
- 
-    // Get value from form element
-    const target = event.target;
-    const text = target.text.value;
- 
-    // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
- 
-    // Clear form
-    target.text.value = '';
-  },
-});
-
-Template.body.helpers({
-  tasks() {
+      tasks() {
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
- 
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  }, 
+});
